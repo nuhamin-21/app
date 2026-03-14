c7baea89276499378a327ee8366af0f66bc84479
nuhamin21 <mastewal256@gmail.com>
2026-03-14 21:52:45 +0300

    our first commit

---
+++ App.js
@@ -0,0 +1,66 @@
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {TextInput } from 'react-native-paper';
+export default function App(){
+  return(
+    <SafeAreaView style={styles.container}>
+     <View style={styles.inputContainer}>
+      <TextInput
+        placeholder = "What would you like to focus ..."
+        mode = {"outlined"}
+        lable = "Focus"
+        style = {styles.InputText}
+      />
+      <TouchableOpacity style={styles.fabButton} onPress={()=>{}}>
+        <Text style={styles.fabText}>+</Text>
+      </TouchableOpacity>
+     </View>
+     <View style={styles.focusedTasks}>
+      <Text style={styles.focusTitle}> Things we've focused on: </Text>
+     <View style={{padding: 20}}>
+      <Text style={{ fontSize: 18 , color: 'white', fontWeight: 'semi-bold'}}> 1, Learn react native </Text>
+      <Text style={{ fontSize: 18 , color: 'white', fontWeight: 'semi-bold'}}> 2, Learn js basics </Text>
+     </View>
+     </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: 'hsla(245, 94%, 21%, 0.76)'
+  },
+  inputContainer:{
+    flexDirection: 'row',
+    padding: 20,
+  },
+  InputText: {
+    flex: 1,
+  },
+  fabButton: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderWidth: 2,
+    borderColor: 'white',
+    marginLeft: 10,
+  },
+  fabText: {
+    fontSize: 20,
+    color: 'white',
+  },
+  focusedTasks: {
+    marginTop: 10,
+    padding: 20,
+  },
+  focusTitle: {
+    fontWeight: 'bold',
+    fontSize: 22,
+    marginLeft: 10,
+    color: 'white',
+  }
+})
